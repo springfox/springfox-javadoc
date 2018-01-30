4a6a415f47b26ee0889c7ee953edd032ba2211e2
Martin Neumann <martin.neumann@be-tse.de>
2018-01-30 14:27:11 +0100

    added documentation

--- src/test/java/springfox/javadoc/doclet/SwaggerPropertiesDocletTest.java
+++ src/test/java/springfox/javadoc/doclet/SwaggerPropertiesDocletTest.java
@@ -20,6 +20,7 @@ import com.sun.javadoc.SourcePosition;
 
 public class SwaggerPropertiesDocletTest {
 
+    /** contains all full paths to relevant directories */
     private static final Properties PROPERTIES = new Properties();
 
     @BeforeClass
@@ -50,6 +51,9 @@ public class SwaggerPropertiesDocletTest {
 
     @Test
     public void testPropertiesGeneration() throws IOException, InterruptedException {
+        // read in the classpath file as a string.
+        // Using the @<file-path> syntax supported by the javadoc tool won't work, if
+        // the classpath contains whitespace.
         FileInputStream fis = new FileInputStream(PROPERTIES.getProperty("classpath.file"));
         StringBuilder classpath = new StringBuilder();
         String line;
@@ -59,6 +63,10 @@ public class SwaggerPropertiesDocletTest {
         }
         classpathReader.close();
 
+        // all the paths are read from the properties file because the javadoc
+        // command-line tool needs full paths.
+        // The properties file contains placeholders that are replaced by the maven
+        // resource filter plugin to convert project-relative paths to full paths.
         StringBuilder command = new StringBuilder();
         command.append(PROPERTIES.getProperty("java.home")).append("/../bin/javadoc ");
         command.append("-doclet springfox.javadoc.doclet.SwaggerPropertiesDoclet ");
@@ -66,16 +74,22 @@ public class SwaggerPropertiesDocletTest {
         command.append(" -sourcepath ").append(PROPERTIES.getProperty("source.path"));
         command.append(" -subpackages ").append(PROPERTIES.getProperty("package.name"));
         command.append(" -classdir ").append(PROPERTIES.getProperty("class.dir"));
+        // enclose the classpath in quotes so that it still works if the classpath
+        // contains whitespace
         command.append(" -classpath ").append("\"").append(classpath.toString()).append("\"");
 
+        // run the javadoc command-line tool to execute the SwaggerPropertiesDoclet on
+        // the classes in the springfox.javadoc.example package
         Process process = Runtime.getRuntime().exec(command.toString());
         process.waitFor();
-        assertEquals(0, process.exitValue());
+        assertEquals(0, process.exitValue()); // make sure there were no errors
 
+        // read in the properties file created by the SwaggerPropertiesDoclet
         InputStream inputStream = SwaggerPropertiesDocletTest.class
                 .getResourceAsStream("/" + SwaggerPropertiesDoclet.SPRINGFOX_JAVADOC_PROPERTIES);
         assertNotNull(inputStream);
 
+        // check that the properties match the example sources
         Properties props = new Properties();
         props.load(inputStream);
         assertEquals("test method", props.getProperty("/test/test.GET.notes"));
---
+++ src/test/java/springfox/javadoc/example/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * contains example REST controller classes that are only used to test (and
+ * therefore demonstrate) how the
+ * {@link springfox.javadoc.doclet.SwaggerPropertiesDoclet} works.
+ */
+package springfox.javadoc.example;
