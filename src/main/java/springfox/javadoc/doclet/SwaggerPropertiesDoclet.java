b1e3dbd77c547c7cf0efdf94881a6be38b237ee9
Dilip Krishnan <D_KRISHNAN@containerstore.com>
2018-02-09 21:34:21 -0600

    Added checkstyle as part of the build

--- src/main/java/springfox/javadoc/doclet/SwaggerPropertiesDoclet.java
+++ src/main/java/springfox/javadoc/doclet/SwaggerPropertiesDoclet.java
@@ -1,22 +1,25 @@
 /*
  *
- * Copyright 2018 the original author or authors.
+ *  Copyright 2018-2019 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
  *
  */
 package springfox.javadoc.doclet;
+
+import com.sun.javadoc.*;
+import springfox.javadoc.plugin.JavadocBuilderPlugin;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -26,16 +29,6 @@ import java.util.Properties;
 
 // the NOSONAR comment is added ignore sonar warning about usage of Sun classes
 // because doclets can only be written using Sun classes
-import com.sun.javadoc.AnnotationDesc;// NOSONAR
-import com.sun.javadoc.ClassDoc;
-import com.sun.javadoc.DocErrorReporter;
-import com.sun.javadoc.MethodDoc;
-import com.sun.javadoc.ParamTag;
-import com.sun.javadoc.RootDoc;
-import com.sun.javadoc.Tag;
-import com.sun.javadoc.ThrowsTag;
-
-import springfox.javadoc.plugin.JavadocBuilderPlugin;
 
 /**
  * Generate properties file based on Javadoc.
--- src/main/java/springfox/javadoc/plugin/JavadocBuilderPlugin.java
+++ src/main/java/springfox/javadoc/plugin/JavadocBuilderPlugin.java
@@ -1,38 +1,31 @@
 /*
  *
- * Copyright 2018 the original author or authors.
+ *  Copyright 2018-2019 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
  *
  */
 package springfox.javadoc.plugin;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-import java.util.HashSet;
-import java.util.Set;
-
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Optional;
-
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.schema.ModelReference;
@@ -44,6 +37,11 @@ import springfox.documentation.spi.service.ParameterBuilderPlugin;
 import springfox.documentation.spi.service.contexts.OperationContext;
 import springfox.documentation.spi.service.contexts.ParameterContext;
 import springfox.javadoc.doclet.SwaggerPropertiesDoclet;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Plugin to generate the @ApiParam and @ApiOperation values from the properties
